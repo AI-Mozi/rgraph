2832bba5530e0c4e709788a44038c84a14c974a1
AI-Mozi <bieniek.milosz0@gmail.com>
2024-04-28 18:49:59 +0200

    Initial commit

---
+++ lib/rgraph/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rgraph
+  VERSION = "0.1.0"
+end
---
+++ spec/rgraph_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Rgraph do
+  it "has a version number" do
+    expect(Rgraph::VERSION).not_to be nil
+  end
+end
