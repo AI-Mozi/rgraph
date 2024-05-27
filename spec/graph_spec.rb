2c661ade8743e3878a7c7380030ff1920cb6515a
AI-Mozi <bieniek.milosz0@gmail.com>
2024-05-27 09:53:23 +0200

    feat: add basic `Graph` spec

---
+++ spec/graph_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe RGraph::Graph do
+  describe ".empty" do
+    subject { described_class.empty(size, directed: directed) }
+
+    let(:size) { 2 }
+
+    context "directed" do
+      let(:directed) { true }
+
+      it "must create an empty grap" do
+        graph = subject 
+
+        expect(graph).to be_a(RGraph::Graph)
+      end
+    end
+
+    context "not directed" do
+      let(:directed) { false }
+
+      it "must create an empty grap" do
+        graph = subject
+
+        expect(graph).to be_a(RGraph::Graph)
+      end
+    end
+  end
+end
