645e8ad69a8007a65bc06f9e437440cc5e9cc84e
AI-Mozi <bieniek.milosz0@gmail.com>
2024-05-29 16:28:37 +0200

    uncomment one context

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
 RSpec.describe RGraph::Graph do
-  # describe ".empty" do
-  #   subject { described_class.empty(size, directed: directed) }
+  describe ".empty" do
+    subject { described_class.empty(size, directed: directed) }
 
-  #   let(:size) { 2 }
+    let(:size) { 2 }
 
-  #   context "directed" do
-  #     let(:directed) { true }
+    # context "directed" do
+    #   let(:directed) { true }
 
-  #     it "must create an empty grap" do
-  #       graph = subject 
+    #   it "must create an empty grap" do
+    #     graph = subject
 
-  #       expect(graph).to be_a(RGraph::Graph)
-  #     end
-  #   end
+    #     expect(graph).to be_a(RGraph::Graph)
+    #   end
+    # end
 
-  #   context "not directed" do
-  #     let(:directed) { false }
+    context "not directed" do
+      let(:directed) { false }
 
-  #     it "must create an empty grap" do
-  #       graph = subject
+      it "must create an empty grap" do
+        graph = subject
 
-  #       expect(graph).to be_a(RGraph::Graph)
-  #     end
-  #   end
-  # end
+        expect(graph).to be_a(RGraph::Graph)
+      end
+    end
+  end
 end
